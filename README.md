d8a18a6a4017dc34910ecda0649972735adf9795
Nguyễn Tuấn Giang <giang@theprojectfactory.com>
2022-12-03 20:37:52 +0700

    Nvim Setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## How to install?
+
+# Simply copy and paste the following in a terminal:
+
+```
+bash <(curl -s https://github.com/ntuangiang/neovim/blob/master/install.sh)
+```
+
+
--- install.sh
+++ install.sh
@@ -1,55 +1,28 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 cd ~
-CWD=$(pwd)/neovim-install
 
-main() {
-   validateInput 
-   install
-}
+CWD=~/.nvim-install
 
-install() {
-    if [[ "$OSTYPE" =~ ^darwin ]]; then
-        brew install git neovim exuberant-ctags
-    fi
+git clone https://github.com/ntuangiang/neovim $CWD/config
+cd $CWD/config
 
-    if [[ "$OSTYPE" =~ ^linux ]]; then
-        sudo apt-get install git neovim exuberant-ctags -y
-    fi
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    brew install git neovim
+    git checkout os/macos
+fi
 
-    if [ -d "$CWD" ]; then
-        rm -rf $CWD
-    fi
+if [[ "$OSTYPE" =~ ^linux ]]; then
+    sudo apt-get install git neovim -y
+    git checkout os/linux
+fi
 
-    git clone https://github.com/ntuangiang/neovim $CWD
-    cp $CWD/init.vim ~/.config/nvim/init.vim
-    cp $CWD/local_init.vim ~/.config/nvim/local_init.vim
-    cp $CWD/local_bundles.vim ~/.config/nvim/local_bundles.vim
+if [[ "$OSTYPE" =~ ^linux ]] | [[ "$OSTYPE" =~ ^darwin ]]; then
+    cp $CWD/config/init.vim ~/.config/nvim/init.vim
+    cp $CWD/config/local_init.vim ~/.config/nvim/local_init.vim
+    cp $CWD/config/local_bundles.vim ~/.config/nvim/local_bundles.vim
+fi
 
-    rm -rf $CWD
-}
+rm -rf $CWD
 
-validateInput() {
-    while [[ "$#" -gt 0 ]]; do
-        case $1 in
-            -h|--help) usage;;
-            *) echo "Unknown parameter passed: $1"; exit 1 ;;
-        esac
-        shift
-    done
-
-}
-
-usage() {
-    echo "
-NAME:
-    ./install.sh - This is install tool description displayed on help message
-
-USAGE:
-    [options and commands] [-- [extra args]]
-
-OPTIONS:
-    -h,--help                          Display this help
-    "
-}
 
